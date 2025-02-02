4b5c474be7e3fbe7b16ddd0c0a187105ad77f45c
Prateek Kher <prateek.kher@gmail.com>
2025-02-02 18:52:03 +0530

    Added Path for CategoryFOrm and List

--- src/app/modules/secured/products/products-routing.module.ts
+++ src/app/modules/secured/products/products-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CategoryFormComponent } from './components/category-form/category-form.component';
 
 const routes: Routes = [
   { path: 'create', component: ProductFormComponent },
   { path: 'update/:id', component: ProductFormComponent },
+  { path: 'create/category', component: CategoryFormComponent },
+  { path: 'update/category/:id', component: CategoryFormComponent },
   { path: 'view', component: ProductListComponent },
 ];
 
