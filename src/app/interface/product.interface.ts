5b3bdd2982ab8823eb9ecdbadbcb3c16ae5cc0f6
Prateek Kher <prateek.kher@gmail.com>
2025-02-02 18:51:32 +0530

    Added CategoryInterface

--- src/app/interface/product.interface.ts
+++ src/app/interface/product.interface.ts
@@ -1,14 +1,25 @@
 import { Entity } from './entity.interface';
 
 export interface Product extends Entity {
-    title: string;
-    rate: number;
-    image_url: string;
-}    
+  title: string;
+  category_id: number;
+  rate: number;
+  image_url: string;
+}
+
+export interface Category extends Entity {
+  title: string;
+}
 
 export const EMPTY_PRODUCT: Product = Object.freeze({
-    id: 0,
-    title: 'EMPTY_PRODUCT',
-    rate: 0,
-    image_url: ''
+  id: 0,
+  title: 'EMPTY_PRODUCT',
+  category_id: 0,
+  rate: 0,
+  image_url: '',
+});
+
+export const EMPTY_CATEGORY: Category = Object.freeze({
+  id: 0,
+  title: '',
 });
