5941b2d44948488338e1136daf5d66d373bbf6be
Prateek Kher <prateek.kher@gmail.com>
2025-02-02 19:05:32 +0530

    Added Category List Route

--- src/app/modules/secured/products/products-routing.module.ts
+++ src/app/modules/secured/products/products-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoryFormComponent } from './components/category-form/category-form.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 
 const routes: Routes = [
   { path: 'create', component: ProductFormComponent },
   { path: 'update/:id', component: ProductFormComponent },
-  { path: 'create/category', component: CategoryFormComponent },
-  { path: 'update/category/:id', component: CategoryFormComponent },
+  { path: 'category/create', component: CategoryFormComponent },
+  { path: 'category/update/:id', component: CategoryFormComponent },
+  { path: 'category/view', component: CategoryListComponent },
   { path: 'view', component: ProductListComponent },
 ];
 
