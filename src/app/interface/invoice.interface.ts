9e6906a92de95da717bc32c927f1f27ae5c8e6d3
Prateek Kher <prateek.kher@gmail.com>
2025-02-09 19:58:50 +0530

    changed from ShopApp to InvoiceApp

--- src/app/interface/invoice.interface.ts
+++ src/app/interface/invoice.interface.ts
@@ -1,46 +1,19 @@
+import { Customer, EMPTY_CUSTOMER } from './customer';
 import { Entity } from './entity.interface';
 
 export interface Invoice extends Entity {
-  kind: 'SALES' | 'PURCHASE';
-  contact_id: number;
-  location_id: number;
-  paid: boolean;
-  gross_amount: number;
-  discount_amount: number;
-  user_id: number;
-  transactions: Transaction[];
+  customer_id: number;
+  customer?: Customer;
+  amount: number;
+  discount: number;
+  payment_method: 'CASH' | 'UPI' | 'ZOMATO' | 'UNPAID';
 }
 
-export interface Transaction extends Entity {
-  invoice_id: number;
-  item_id: number;
-  item_type: 'PRODUCT' | 'LEDGER' | 'BUNDLE';
-  user_id: number;
-  quantity: number;
-  rate: number;
-  transactions?: Transaction[];
-}
-
-export const BASE_TRANSACTION: Transaction = Object.freeze({
+export const EMPTY_INVOICE: Invoice = Object.freeze({
   id: 0,
-  invoice_id: 0,
-  item_id: 0,
-  item_type: 'PRODUCT',
-  user_id: 0,
-  quantity: 0,
-  rate: 0,
-});
-
-export const BASE_INVOICE: Invoice = Object.freeze({
-  id: 0,
-  contact_id: 0,
-  user_id: 0,
-  paid: false,
-  gross_amount: 0,
-  discount_amount: 0,
-  kind: 'SALES',
-  location_id: 0,
-  created_at: '',
-  updated_at: '',
-  transactions: [],
+  customer_id: 0,
+  customer: EMPTY_CUSTOMER,
+  amount: 0,
+  discount: 0,
+  payment_method: 'UNPAID',
 });
